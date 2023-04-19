325fafa2e1ea68987536f0a225cbd7059b6944d4
realRurichan <rurichan@disroot.org>
2023-04-19 23:24:14 +0800

    add: standard func

---
+++ tsticker-api/bot.py
@@ -0,0 +1,65 @@
+import aiohttp
+import asyncio
+import urllib.parse
+from sys import exit as sysexit
+from PIL import Image
+import io
+
+
+class TelegramBotTokenError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class Bot:
+    def __init__(self, token: str):
+        self.token = token
+        if self.token == None:
+            raise TelegramBotTokenError("No Telegram Bot Token")
+        self.api = f"https://api.telegram.org/bot{self.token}/"
+
+    async def initialize(self):
+        verify = await self.doAPIReq("getMe", {})
+        if verify is not None and verify["ok"]:
+            pass
+        else:
+            raise TelegramBotTokenError("Invalid token.")
+
+    async def fetch(self, url: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return await response.json()
+
+    async def doAPIReq(self, func: str, params: dict):
+        req = f"{self.api}{func}?{urllib.parse.urlencode(params)}"
+        return await self.fetch(req)
+
+    async def downloadFile(self, url: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.read()
+                return data
+
+    def convertImageFormat(self, image_input, image_output):
+        with io.BytesIO(image_input) as image_input:
+            with Image.open(image_input) as img:
+                img.save(image_output)
+
+    async def getSticker(self, file_id: str, filename: str):
+        res = await self.doAPIReq("getFile", {"file_id": file_id})
+        if res is not None and res["ok"]:
+            pass
+        else:
+            error = {
+                "code": res["error_code"],
+                "description": res["description"],
+            }
+            return error
+        res = res["result"]
+        sticker_link = (
+            f"https://api.telegram.org/file/bot{self.token}/{res['file_path']}"
+        )
+        sticker = await self.downloadFile(sticker_link)
+        dictionary = f"stickers/{filename}"
+        self.convertImageFormat(sticker, dictionary)
+        return True
---
+++ tsticker-api/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_from_directory
+from bot import Bot
+from dotenv import load_dotenv
+import json
+import re
+import os
+
+app = Flask(__name__)
+load_dotenv()
+bot_token = os.getenv("TOKEN")
+bot = Bot(bot_token)
+
+
+@app.route("/stickers/<filename>")
+async def get_sticker(filename):
+    await bot.initialize()
+    print(filename)
+    pattern = r"^[^.]+\.(jpg|jpeg|png|webp)$"
+    match = re.match(pattern, filename)
+    if match:
+        file_id, ext = filename.split(".")
+    else:
+        error = {"code": "400", "description": "Bad Request: invalid filename"}
+        return json.dumps(error)
+    result = await bot.getSticker(file_id=file_id, filename=filename)
+    if result:
+        return send_from_directory("stickers", filename)
+    else:
+        return json.dumps(result)
